cb212feb541f7a7bb6e9a0e1ec3c272fb6adafcc
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-24 13:53:20 -0800

    trying commit from intelliJ IDEA

--- src/JavaProgrammingI/Part1/_02/AdaLovelace.java
+++ src/JavaProgrammingI/Part1/_02/AdaLovelace.java
@@ -1,2 +1,7 @@
-package JavaProgrammingI.Part1._02;public class AdaLovelace {
+package JavaProgrammingI.Part1._02;
+
+public class AdaLovelace {
+    public static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
 }
