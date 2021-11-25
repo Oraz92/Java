e60a313e285f3703baf1a85f48ae9fde2b31393a
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-25 13:27:35 -0800

    Strings, concatenation

---
+++ src/JavaProgrammingI/Part1/_03/HiAdaLovelace.java
@@ -0,0 +1,9 @@
+package JavaProgrammingI.Part1._03;
+
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace" + "!";
+
+        System.out.println("Hi " + name);
+    }
+}
