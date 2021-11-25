b5c827f92fddbe5d795d05e1fd55f7fde712c547
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-25 13:58:50 -0800

    Working with strings

--- src/JavaProgrammingI/Part1/_03/MessageThreeTimes.java
+++ src/JavaProgrammingI/Part1/_03/MessageThreeTimes.java
@@ -1,2 +1,14 @@
-package JavaProgrammingI.Part1._03;public class MessageThreeTimes {
+package JavaProgrammingI.Part1._03;
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write a string: ");
+        String message = scanner.nextLine();
+
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
+    }
 }
