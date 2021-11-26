f258e0742d8502a1836861e66afa97e1a7573b8b
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-26 15:36:44 -0800

    Practicing an equal sign '=='

---
+++ src/JavaProgrammingI/Part1/_06_ConditionalStatementsAndOperations/Orwell.java
@@ -0,0 +1,13 @@
+package JavaProgrammingI.Part1._06_ConditionalStatementsAndOperations;
+import java.util.Scanner;
+
+public class Orwell {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(scanner.nextLine());
+        if (number == 1984) {
+            System.out.println("Orwell");
+        }
+    }
+}
