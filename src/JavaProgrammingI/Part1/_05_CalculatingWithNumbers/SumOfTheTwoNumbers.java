15587341dbbc9fa64fdd6c8dc46ccb6e46bc3591
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-25 16:17:29 -0800

    Mixed up converting types from string via valueOf

--- src/JavaProgrammingI/Part1/_05_CalculatingWithNumbers/SumOfTheTwoNumbers.java
+++ src/JavaProgrammingI/Part1/_05_CalculatingWithNumbers/SumOfTheTwoNumbers.java
@@ -1,2 +1,13 @@
-package JavaProgrammingI.Part1._05_CalculatingWithNumbers;public class SumOfTheTwoNumbers {
+package JavaProgrammingI.Part1._05_CalculatingWithNumbers;
+import java.util.Scanner;
+
+public class SumOfTheTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int number1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int nubmer2 = Integer.valueOf(scanner.nextLine());
+        System.out.println("The sum of the numbers " + (number1 + nubmer2));
+    }
 }
