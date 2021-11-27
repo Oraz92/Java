7c8c5581e44dc181bcb87067ab4b3f2a09cee63b
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-26 17:47:43 -0800

    Introducing to strings, and working with 'equals' method.

---
+++ src/JavaProgrammingI/Part1/_06_ConditionalStatementsAndOperations/Same.java
@@ -0,0 +1,18 @@
+package JavaProgrammingI.Part1._06_ConditionalStatementsAndOperations;
+
+import java.util.Scanner;
+
+public class Same {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String firstString = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String secondString = scanner.nextLine();
+        if (firstString.equals(secondString)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+    }
+}
