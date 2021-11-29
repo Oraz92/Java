318551bf222a4ea32c29df1ca68d613b0c9a89be
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-29 10:45:02 -0800

    Repeating, in this case returning sqruared number.

---
+++ src/JavaProgrammingI/Part2/_01_RecurringProblems/Squared.java
@@ -0,0 +1,10 @@
+package JavaProgrammingI.Part2._01_RecurringProblems;
+import java.util.Scanner;
+
+public class Squared {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int integer = Integer.parseInt(scanner.nextLine());
+        System.out.println(integer * integer);
+    }
+}
