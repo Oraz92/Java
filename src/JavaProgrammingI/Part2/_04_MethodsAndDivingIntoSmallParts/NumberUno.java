ebdb2b03197bef7732322656f8e4609352ea1639
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-30 16:42:21 -0800

    Methods that return a value

---
+++ src/JavaProgrammingI/Part2/_04_MethodsAndDivingIntoSmallParts/NumberUno.java
@@ -0,0 +1,16 @@
+package JavaProgrammingI.Part2._04_MethodsAndDivingIntoSmallParts;
+
+
+/*
+Write a method public static int numberUno() that returns the value 1.
+ */
+public class NumberUno {
+    public static void main(String[] args) {
+        int number = numberUno();
+        System.out.println(number);
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}
