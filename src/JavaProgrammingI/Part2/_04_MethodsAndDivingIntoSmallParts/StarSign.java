a4e6eb70d477ba6ee0a45e4d7ccc3b556cf3137f
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-30 18:10:58 -0800

    practice with return operator

---
+++ src/JavaProgrammingI/Part2/_04_MethodsAndDivingIntoSmallParts/StarSign.java
@@ -0,0 +1,47 @@
+package JavaProgrammingI.Part2._04_MethodsAndDivingIntoSmallParts;
+
+/*
+Printing different figures
+ */
+public class StarSign {
+    public static void main(String[] args) {
+        printStars(3);
+        printSquare(4);
+        printRectange(17, 3);
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printRectange(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
