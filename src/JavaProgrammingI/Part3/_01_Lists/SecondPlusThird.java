9f9ac6084961574a2e5cd5173b0e9dc6d4d7526c
Oraz Jumabayev <oraz_delight@mail.ru>
2021-11-30 20:14:09 -0800

    Adding elements to list, and printing the sum of the second and third elements

---
+++ src/JavaProgrammingI/Part3/_01_Lists/SecondPlusThird.java
@@ -0,0 +1,20 @@
+package JavaProgrammingI.Part3._01_Lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SecondPlusThird {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+
+            list.add(number);
+        }
+        System.out.println(list.get(1) + list.get(2));
+    }
+}
