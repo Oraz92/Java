2619fd8eb5b0073539590da2991f72f2a1c2f566
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-01 17:55:43 -0800

    Getting first and last elements of the list

---
+++ src/JavaProgrammingI/Part3/_01_Lists/FirstAndLast.java
@@ -0,0 +1,20 @@
+package JavaProgrammingI.Part3._01_Lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.equals("")) {
+                break;
+            }
+            list.add(name);
+        }
+        System.out.println(list.get(0));
+        System.out.println(list.get(list.size() - 1));
+    }
+}
