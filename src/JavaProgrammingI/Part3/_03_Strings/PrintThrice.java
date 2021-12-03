27fa094432b5a626d3657aca259e5f538fe3ff8d
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-03 11:04:19 -0800

    Working with strings

---
+++ src/JavaProgrammingI/Part3/_03_Strings/PrintThrice.java
@@ -0,0 +1,14 @@
+package JavaProgrammingI.Part3._03_Strings;
+
+import java.util.Scanner;
+
+public class PrintThrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give a word: ");
+        String word = scanner.nextLine();
+        for (int i = 0; i < 3; i++) {
+            System.out.print(word);
+        }
+    }
+}
