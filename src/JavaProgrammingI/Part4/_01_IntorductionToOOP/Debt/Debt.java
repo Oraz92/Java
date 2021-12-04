b94ed094cb1a98cfee08e70fbcc865aa0e95a4e8
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-03 23:08:57 -0800

    adding methods

---
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Debt/Debt.java
@@ -0,0 +1,19 @@
+package JavaProgrammingI.Part4._01_IntorductionToOOP.Debt;
+
+public class Debt {
+    private double balance;
+    private double interestRate;
+
+    public Debt(double initialBalance, double initialInterestRate) {
+        this.balance = initialBalance;
+        this.interestRate = initialInterestRate;
+    }
+
+    public void printBalance() {
+        System.out.println(this.balance);
+    }
+
+    public void waitOneYear() {
+        this.balance *= this.interestRate;
+    }
+}
