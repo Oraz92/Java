9086df45facaded67196607ece52d580e0f4aa17
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-03 23:01:55 -0800

    adding methods and

---
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/DecreasingCounter/Main.java
@@ -0,0 +1,27 @@
+package JavaProgrammingI.Part4._01_IntorductionToOOP.DecreasingCounter;
+
+public class Main {
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(2);
+        DecreasingCounter counter1 = new DecreasingCounter(100);
+
+        /*counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();*/
+
+        counter1.printValue();
+
+        counter1.reset();
+        counter1.printValue();
+
+        counter1.decrement();
+        counter1.printValue();
+    }
+}
