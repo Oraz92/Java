dba22617d30181325c858c5960266c42b1386f20
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-05 14:13:32 -0800

    Adding new objects and count even, odd numbers

--- src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Statistics/MainProgram.java
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Statistics/MainProgram.java
@@ -15,13 +15,23 @@ public class MainProgram {
         System.out.println("Average: " + statistics.average());*/
         Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        Statistics statisticsOdd = new Statistics();
         while (true) {
             int number = Integer.parseInt(scanner.nextLine());
             if (number == -1) {
                 break;
             }
             statistics.addNumber(number);
+            if (number % 2 == 0) {
+                statisticsEven.addNumber(number);
+            } else {
+                statisticsOdd.addNumber(number);
+            }
         }
         System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum: of odd numbers: " + statisticsOdd.sum());
+
     }
 }
