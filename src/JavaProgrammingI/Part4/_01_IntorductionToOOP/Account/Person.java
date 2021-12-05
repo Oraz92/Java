0c657ecec400c8b9783b3a6e7c4718a81c5f4da4
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-05 11:42:04 -0800

    Adding setter and getter

--- src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Account/Person.java
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Account/Person.java
@@ -3,10 +3,27 @@ package JavaProgrammingI.Part4._01_IntorductionToOOP.Account;
 public class Person {
     private String name;
     private int age;
+    private int weight;
+    private int height;
 
     public Person(String initialName) {
         this.age = 0;
+        this.weight = 0;
+        this.height = 0;
         this.name = initialName;
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heightPerHundred = this.height / 100.0;
+        return this.weight / (heightPerHundred * heightPerHundred);
     }
 
     public void printPerson() {
