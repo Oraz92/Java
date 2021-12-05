55791cb51240cd2691ce1391030de04f8988aa2c
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-05 14:19:38 -0800

    Adding and printing balance

---
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/PaymentCard/MainProgram.java
@@ -0,0 +1,8 @@
+package JavaProgrammingI.Part4._01_IntorductionToOOP.PaymentCard;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
+    }
+}
