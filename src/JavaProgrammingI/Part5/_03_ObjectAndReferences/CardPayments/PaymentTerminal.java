d1c8bae6bb126590de9b3201c62b00451fa3e915
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-07 19:58:35 -0800

    in PaymentTerminal class, adding logic, working wiht cash and returning change

--- src/JavaProgrammingI/Part5/_03_ObjectAndReferences/CardPayments/PaymentTerminal.java
+++ src/JavaProgrammingI/Part5/_03_ObjectAndReferences/CardPayments/PaymentTerminal.java
@@ -1,4 +1,36 @@
 package JavaProgrammingI.Part5._03_ObjectAndReferences.CardPayments;
 
 public class PaymentTerminal {
+    private double money;
+    private int affordableMeals;
+    private int heartyMeals;
+
+    public PaymentTerminal() {
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
+    }
+
+    public double eatAffordably(double payment) {
+        if (payment - 2.5 >= 0) {
+            this.money += 2.5;
+            this.affordableMeals++;
+            return payment - 2.5;
+        }
+        return payment;
+
+    }
+
+    public double eatHeartily(double payment) {
+        if (payment - 4.3 >= 0) {
+            this.money += 4.3;
+            this.heartyMeals++;
+            return payment - 4.3;
+        }
+        return payment;
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
 }
