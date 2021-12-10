3389e2ad3858a1041b3c1b9e80db8e7d87d4ba5f
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-09 19:00:26 -0800

    Added methods 'advance()' and 'advance(int)' which increases date correcty

--- src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Account/SimpleDate.java
+++ src/JavaProgrammingI/Part4/_01_IntorductionToOOP/Account/SimpleDate.java
@@ -23,31 +23,52 @@ public class SimpleDate {
         return this.year;
     }
 
+    public void advance() {
+        // Checking days
+        if (this.day < 30) {
+            this.day = this.getDay() + 1;
+        } else {
+            this.day = 1;
+            // Checking month
+            if (this.month < 12) {
+                this.month = this.getMonth() + 1;
+            } else {
+                this.month = 1;
+                this.year = this.getYear() + 1;
+            }
+        }
+    }
+
+    public void advance(int howManyDays) {
+        if (this.day + howManyDays < 30) {
+            this.day = this.getDay() + howManyDays;
+        } else {
+            int day = (this.getDay() + howManyDays) % 30;
+            int month = (this.getDay() + howManyDays) / 30;
+            while (true) {
+                if (this.month == month && this.day == day) {
+                  break;
+                }
+                advance();
+            }
+        }
+    }
+
+
     public boolean before(SimpleDate compared) {
         // first compare years
         if (this.year < compared.year) {
             return true;
         }
 
-        if (this.year > compared.year) {
-            return false;
-        }
-
-        // years are same, compare months
-        if (this.month < compared.month) {
+        if (this.year == compared.year && this.month < compared.month) {
             return true;
         }
 
-        if (this.month > compared.month) {
-            return false;
-        }
-
-        // years and months are same, compare days
-        if (this.day < compared.day) {
-            return true;
-        }
-
-        return false;
+        // the years and the months are the same, compare days
+        return this.year == compared.year &&
+                this.month == compared.month &&
+                this.day < compared.day;
     }
 
     public boolean equals(Object compared) {
@@ -57,26 +78,33 @@ public class SimpleDate {
         }
 
         // if the type of the compared object is not SimpleDate, the objects are not equal
-        if (!(compared instanceof SimpleDate)) {
+        // convert the Object type compared object
+        // into a SimpleDate type object called comparedSimpleDate
+        if (!(compared instanceof SimpleDate comparedSimpleDate)) {
             return false;
         }
 
-        // convert the Object type compared object
-        // into a SimpleDate type object called comparedSimpleDate
-        SimpleDate comparedSimpleDate = (SimpleDate) compared;
-
-        // if the values of the object variables are the same, the objects are equal
-        if (this.day == comparedSimpleDate.day &&
+        // otherwise, the objects are not equal
+        return this.day == comparedSimpleDate.day &&
                 this.month == comparedSimpleDate.month &&
-                this.year == comparedSimpleDate.year) {
-            return true;
-        }
+                this.year == comparedSimpleDate.year;
+    }
 
-        // otherwise the objects are not equal
-        return false;
+    public String print(int value) {
+        if (value < 10) {
+            return "0" + value;
+        }
+        return "" + value;
     }
 
     public String toString() {
-        return this.day + "." + this.month + "." + this.year;
+        return print(this.day) + "." + print(this.month) + "." + print(this.year);
+    }
+
+    public static void main(String[] args) {
+        SimpleDate date = new SimpleDate(19, 12, 2021);
+        System.out.println("Friday of the examined week is " + date);
+        date.advance(181);
+        System.out.println(date);
     }
 }
