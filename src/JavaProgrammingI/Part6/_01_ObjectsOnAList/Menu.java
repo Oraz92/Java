0529afb779487e725182e626b3cc10229c8aafbf
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-12 15:34:38 -0800

    Adding and removing abjects from a list

---
+++ src/JavaProgrammingI/Part6/_01_ObjectsOnAList/Menu.java
@@ -0,0 +1,43 @@
+package JavaProgrammingI.Part6._01_ObjectsOnAList;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<String> meals;
+
+    public Menu() {
+        this.meals = new ArrayList<>();
+    }
+
+    public void addMeal(String meal) {
+        this.meals.add(meal);
+    }
+
+    public void clearMenu() {
+        int size = this.meals.size();
+        for (int i = 0; i < size ; i++) {
+            this.meals.remove(this.meals.size() - 1);
+        }
+    }
+
+    public void printMeals() {
+        for (String meal: meals) {
+            System.out.println(meal);
+        }
+    }
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.addMeal("Tofu ratatouille");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Meatballs with mustard sauce");
+
+        menu.printMeals();
+        menu.clearMenu();
+
+        System.out.println();
+        menu.addMeal("Tomato and mozzarella salad");
+        menu.printMeals();
+    }
+}
