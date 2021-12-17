d38f27a31b37064182acac94592f817c68a797c8
Oraz Jumabayev <oraz_delight@mail.ru>
2021-12-16 19:39:12 -0800

    separating user interface from program logic

---
+++ src/JavaProgrammingI/Part6/_02_SeparatingTheUserInterface/UserInterface/UserInterface.java
@@ -0,0 +1,26 @@
+package JavaProgrammingI.Part6._02_SeparatingTheUserInterface.UserInterface;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private WordSet wordSet;
+    private Scanner scanner;
+
+    public UserInterface(WordSet wordSet, Scanner scanner) {
+        this.scanner = scanner;
+        this.wordSet = wordSet;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Entere a word: ");
+            String word = scanner.nextLine();
+
+            if (this.wordSet.alreadyEntered(word)) {
+                break;
+            }
+            this.wordSet.add(word);
+        }
+        System.out.println("You gave the same word twice!");
+    }
+}
