52ac607c0140640f9f21b1a17903af3c399aa850
Oraz Jumabayev <oraz_delight@mail.ru>
2022-01-21 23:54:00 -0800

    Selective sorting algorithm

--- src/JavaProgrammingI/Part7/_02_Algorithms/Sort.java
+++ src/JavaProgrammingI/Part7/_02_Algorithms/Sort.java
@@ -2,4 +2,6 @@ package JavaProgrammingI.Part7._02_Algorithms;
 
 public class Sort {
 
+
+
 }
--- src/JavaProgrammingI/Part7/_02_Algorithms/Sorting.java
+++ src/JavaProgrammingI/Part7/_02_Algorithms/Sorting.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 
 public class Sorting {
     public static void main(String[] args) {
-        int[] array = {3, 2, 5, 4, 8};
+        int[] array = {8, 3, 7, 14, 9, 1, 2, 4, 22, 0, 11};
+
+        System.out.println(Arrays.toString(array));
+        System.out.println();
+        sort(array);
         System.out.println(Arrays.toString(array));
 
-        swap(array, 1, 0);
-        System.out.println(Arrays.toString(array));
-
-        swap(array, 0, 3);
-        System.out.println(Arrays.toString(array));
     }
 
     public static int smallest(int[] array) {
@@ -37,7 +36,7 @@ public class Sorting {
 
     public static int indexOfSmallestFrom(int[] array, int startIndex) {
         int smallest = array[startIndex + 1];
-        int indexOfSmallestAfterStart = startIndex;
+        int indexOfSmallestAfterStart = startIndex + 1;
         for (int i = startIndex + 2; i < array.length; i++) {
             if (array[i] < smallest) {
                 smallest = array[i];
@@ -51,5 +50,21 @@ public class Sorting {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
+    }
+
+    public static void sort(int[] array) {
+        int mainIteration = 0;
+        while (mainIteration < array.length - 2) {
+            int firstIndexAtEachMainIteration = mainIteration + 1;
+            if (mainIteration == 0) {
+                int firstSmallElement = indexOfSmallest(array);
+                swap(array, mainIteration, firstSmallElement);
+                //System.out.println(Arrays.toString(array));
+            }
+            int smallestIndexFrom = indexOfSmallestFrom(array,mainIteration);
+            swap(array, firstIndexAtEachMainIteration, smallestIndexFrom);
+            //System.out.println(Arrays.toString(array));
+            mainIteration++;
+        }
     }
 }
