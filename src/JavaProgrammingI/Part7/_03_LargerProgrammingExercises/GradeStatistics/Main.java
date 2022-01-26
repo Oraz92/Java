e00da05f38bc8718d1ea7732e6cfc89819ec6dd2
Oraz Jumabayev <oraz_delight@mail.ru>
2022-01-25 20:38:53 -0800

    Was added passing grades percentage and toString method.

--- src/JavaProgrammingI/Part7/_03_LargerProgrammingExercises/GradeStatistics/Main.java
+++ src/JavaProgrammingI/Part7/_03_LargerProgrammingExercises/GradeStatistics/Main.java
@@ -1,5 +1,22 @@
 package JavaProgrammingI.Part7._03_LargerProgrammingExercises.GradeStatistics;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Statictics tcs = new Statictics();
+
+        System.out.println("Enter points total, -1 stops:");
+        int input;
+        while (true) {
+            input =  Integer.parseInt(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            tcs.add(input);
+        }
+        //System.out.println(tcs);
+
+    }
 }
--- src/JavaProgrammingI/Part7/_03_LargerProgrammingExercises/GradeStatistics/Statictics.java
+++ src/JavaProgrammingI/Part7/_03_LargerProgrammingExercises/GradeStatistics/Statictics.java
@@ -1,6 +1,7 @@
 package JavaProgrammingI.Part7._03_LargerProgrammingExercises.GradeStatistics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Statictics {
     private ArrayList<Integer> points;
@@ -26,14 +27,32 @@ public class Statictics {
     public int averageForPassingGrades(ArrayList<Integer> points) {
         int sum = 0;
         for (int point: points) {
-            if (passingGrade(point)) {
+            if (isGradePass(point)) {
                 sum += point;
             }
         }
         return sum / points.size();
     }
 
-    public boolean passingGrade(int grade) {
+    public boolean isGradePass(int grade) {
         return grade >= 50;
+    }
+
+    public int passingPercentage(ArrayList<Integer> points) {
+        ArrayList<Integer> passedPoints = new ArrayList<>();
+        for (int point: points) {
+            if (isGradePass(point)) {
+                passedPoints.add(point);
+            }
+        }
+        return (passedPoints.size() / points.size()) * 100;
+    }
+
+    public String toString() {
+        String result = "";
+        for (int value: this.points) {
+            result += value + " ";
+        }
+        return result;
     }
 }
