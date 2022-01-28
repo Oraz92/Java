a2529cdea31c995ccf7f2bc3e2bea7ae0ca4a625
Oraz Jumabayev <oraz_delight@mail.ru>
2022-01-28 05:49:42 -0800

    Liquid containers program using, deleted limit variable

--- src/JavaProgrammingII/Part8/_01_ShortRecap/LiquidContainters/Main.java
+++ src/JavaProgrammingII/Part8/_01_ShortRecap/LiquidContainters/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int limit = 100;
         int firstContainer = 0;
         int secondContainer = 0;
         String command;
