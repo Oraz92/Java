5f935ffd79e1ac81a28aa228b64b03c9a837e5ed
Oraz Jumabayev <oraz_delight@mail.ru>
2022-01-30 10:01:19 -0800

    Training to override hashCode() method

--- src/JavaProgrammingII/Part8/_02_HashMap/SameDate/SimpleDate.java
+++ src/JavaProgrammingII/Part8/_02_HashMap/SameDate/SimpleDate.java
@@ -1,5 +1,7 @@
 package JavaProgrammingII.Part8._02_HashMap.SameDate;
 
+import java.util.Objects;
+
 public class SimpleDate {
 
     private int day;
@@ -65,5 +67,12 @@ public class SimpleDate {
         return this.year == comparedDate.year
                 && this.month == comparedDate.month
                 && this.day == comparedDate.day;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(day);
+        result = 31 * result + Objects.hash(month, year);
+        return result;
     }
 }
