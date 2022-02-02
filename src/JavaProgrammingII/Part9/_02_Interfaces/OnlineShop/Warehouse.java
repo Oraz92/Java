2bbd6cbab97e8fd60a3681e4221ad8185130d8bc
Oraz Jumabayev <oraz_delight@mail.ru>
2022-02-02 03:16:43 -0800

    Created Warehouse class, and methods: addProduct, price, stock and take

---
+++ src/JavaProgrammingII/Part9/_02_Interfaces/OnlineShop/Warehouse.java
@@ -0,0 +1,47 @@
+package JavaProgrammingII.Part9._02_Interfaces.OnlineShop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Warehouse {
+
+    private Map<String, Integer> productPriceList;
+    private Map<String, Integer> productStockList;
+
+    public Warehouse() {
+        this.productPriceList = new HashMap<>();
+        this.productStockList = new HashMap<>();
+    }
+
+    public void addProduct(String product, int price, int stock) {
+        this.productPriceList.put(product, price);
+        this.productStockList.put(product, stock);
+    }
+
+    public int price(String product) {
+        if (this.productPriceList.containsKey(product)) {
+            return this.productPriceList.get(product);
+        }
+        return -99;
+    }
+
+    public int stock(String product) {
+        if (this.productStockList.containsKey(product)) {
+            return this.productStockList.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product) {
+        if (this.productStockList.containsKey(product)) {
+            int quantityOfProduct = this.productStockList.get(product);
+            if (quantityOfProduct > 0) {
+                quantityOfProduct--;
+                this.productStockList.put(product, quantityOfProduct);
+                return true;
+            }
+        }
+        return false;
+    }
+}
