5c1c0004d31c8296c321a4c77da24cc48152b23a
Oraz Jumabayev <oraz_delight@mail.ru>
2022-02-26 17:01:40 -0800

    Working with Stream methods

---
+++ src/JavaProgrammingII/Part10/HandlingCollectionsAsStreams/Divisible.java
@@ -0,0 +1,32 @@
+package JavaProgrammingII.Part10.HandlingCollectionsAsStreams;
+
+/*
+The exercise template includes a template for the method public static ArrayList<Integer> divisible(ArrayList<Integer> numbers).
+Implement a functionality there that gathers numbers divisible by two, three or five from the list it receives as a parameter,
+and returns them as a new list. The list received as a parameter must not be altered.
+ */
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Divisible {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(-17);
+        numbers.add(-5);
+        numbers.add(7);
+
+        ArrayList<Integer> divisible = divisible(numbers);
+        divisible.stream().forEach(number -> System.out.println(number));
+
+    }
+
+    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
+        return numbers.stream().
+                filter(number -> number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
