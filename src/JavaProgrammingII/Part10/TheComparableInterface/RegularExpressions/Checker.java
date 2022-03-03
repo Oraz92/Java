ebe49cdce759734facbbdd87a0202ae0c04dba48
Oraz Jumabayev <oraz_delight@mail.ru>
2022-03-02 22:53:02 -0800

    Added allVowels and timeOfDay methods

--- src/JavaProgrammingII/Part10/TheComparableInterface/RegularExpressions/Checker.java
+++ src/JavaProgrammingII/Part10/TheComparableInterface/RegularExpressions/Checker.java
@@ -1,13 +1,25 @@
 package JavaProgrammingII.Part10.TheComparableInterface.RegularExpressions;
 
 /*
-Day of week
+1. Day of week
 Use regular expressions to create the method public boolean isDayOfWeek(String string), which returns true if the
 parameter string is an abbreviation of a day of the week (mon, tue, wed, thu, fri, sat, sun)
+
+2. For simplicity's sake, in this exercises the letters that are considered vowels are: a, e, i, o, and u.
+
+3. use the 24-hour clock. So the acceptable values are between 00:00:00 and 23:59:59.
  */
 
 public class Checker {
     public boolean isDayOfWeek(String string) {
         return string.matches("^(mon|t(ue|hu)|wed|fri|s(at|un))");
+    }
+
+    public boolean allVowels(String string) {
+        return string.matches("^[aeiou]*");
+    }
+
+    public boolean timeOFDay(String string) {
+        return string.matches("([0-1]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])");
     }
 }
--- src/JavaProgrammingII/Part10/TheComparableInterface/RegularExpressions/Main.java
+++ src/JavaProgrammingII/Part10/TheComparableInterface/RegularExpressions/Main.java
@@ -8,11 +8,28 @@ public class Main {
         System.out.print("Enter a string: ");
         String pattern = scanner.nextLine();
 
-        Checker checker = new Checker();
-        if (checker.isDayOfWeek(pattern)) {
+        /*Checker dayOfWeek = new Checker();
+        if (dayOfWeek.isDayOfWeek(pattern)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }*/
+
+
+       /* Checker vowels = new Checker();
+        if (vowels.allVowels(pattern)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }*/
+
+        Checker time = new Checker();
+        if (time.timeOFDay(pattern)) {
             System.out.println("The form is correct.");
         } else {
             System.out.println("The form is incorrect.");
         }
+
+
     }
 }
