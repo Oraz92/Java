1a26fef937a6d41e3aafcb7b10fb5c1263ed5394
Oraz Jumabayev <oraz_delight@mail.ru>
2022-03-03 13:17:47 -0800

    Class diagram, relation between two classes

---
+++ src/JavaProgrammingII/Part11/ClassDiagrams/StudentAndUniversity/Student.java
@@ -0,0 +1,7 @@
+package JavaProgrammingII.Part11.ClassDiagrams.StudentAndUniversity;
+
+public class Student {
+
+    private String name;
+    private int studentID;
+}
---
+++ src/JavaProgrammingII/Part11/ClassDiagrams/StudentAndUniversity/University.java
@@ -0,0 +1,9 @@
+package JavaProgrammingII.Part11.ClassDiagrams.StudentAndUniversity;
+
+import java.util.ArrayList;
+
+public class University {
+
+    private String name;
+    private ArrayList<Student> students;
+}
